b0a0cfb36ba555044d50e52396692a67c1f630e3
Fraser Scott <fraserdscott@gmail.com>
2023-04-01 20:54:38 +0200

    feat: three.js layers

--- packages/client/src/GameBoard.tsx
+++ packages/client/src/GameBoard.tsx
@@ -1,10 +1,13 @@
-import React from "react";
+import React, { useRef, useState } from "react";
+import * as THREE from "three";
+import { Canvas, Color, ThreeElements, useThree } from "@react-three/fiber";
 import { useEntityQuery } from "@latticexyz/react";
 import { getComponentValueStrict, Has } from "@latticexyz/recs";
 import { useMUD } from "./MUDContext";
 
 const N_LAYERS = 2;
 const WIDTH = 10;
+const HEIGHT = 10;
 
 const Tile = ({ index, x, y }: { index: number; x: number; y: number }) => {
   const {
@@ -55,6 +58,83 @@ const Tile = ({ index, x, y }: { index: number; x: number; y: number }) => {
   );
 };
 
+function Player(
+  props: ThreeElements["mesh"] & { index: number; x: number; y: number }
+) {
+  const {
+    world,
+    worldSend,
+    components: { TileTable },
+    network: { signer },
+  } = useMUD();
+
+  const tiles = useEntityQuery([Has(TileTable)]);
+
+  const tile = tiles.find((t) => {
+    const arr = world.entities[t].split(":");
+    const i = parseInt(arr[0]);
+    const xT = parseInt(arr[1]);
+    const yT = parseInt(arr[2]);
+
+    return i === props.index && xT === props.x && yT === props.y;
+  });
+
+  const ref = useRef<THREE.Mesh>(null!);
+  return (
+    <mesh
+      onClick={async (event) => {
+        event.stopPropagation();
+        // Create a World contract instance
+        const s = signer.get();
+        if (!s) throw new Error("No signer");
+
+        const txResult = await worldSend("flip", [
+          props.index,
+          props.x,
+          props.y,
+          tile ? !getComponentValueStrict(TileTable, tile).value : true,
+        ]);
+        await txResult.wait();
+      }}
+      {...props}
+      ref={ref}
+    >
+      <boxGeometry args={[0.9, 0.9, 0.9]} />
+      {tile && getComponentValueStrict(TileTable, tile).value ? (
+        <meshStandardMaterial color={"blue"} />
+      ) : (
+        <meshStandardMaterial color={"red"} />
+      )}
+    </mesh>
+  );
+}
+
+function Scene() {
+  const {} = useMUD();
+
+  return (
+    <group>
+      <ambientLight />
+      <pointLight position={[10, 10, 10]} />
+      <group>
+        {[...Array(N_LAYERS).keys()].map((index) =>
+          [...Array(WIDTH).keys()].map((x) =>
+            [...Array(HEIGHT).keys()].map((y) => (
+              <Player
+                key={`${x},${y}`}
+                position={[x, index * 5 - 5, y]}
+                x={x}
+                y={y}
+                index={index}
+              />
+            ))
+          )
+        )}
+      </group>
+    </group>
+  );
+}
+
 export const GameBoard = () => {
   const {
     worldSend,
@@ -62,23 +142,7 @@ export const GameBoard = () => {
   } = useMUD();
 
   return (
-    <div style={{ display: "flex", flexDirection: "column" }}>
-      <div style={{ display: "flex" }}>
-        {[...Array(N_LAYERS).keys()].map((i) => (
-          <div style={{ margin: 10 }}>
-            <div>Layer {i}</div>
-            <table key={`${i}`}>
-              {[...Array(WIDTH).keys()].map((x) => (
-                <tr>
-                  {[...Array(WIDTH).keys()].map((y) => (
-                    <Tile key={`${x},${y}`} index={i} x={x} y={y} />
-                  ))}
-                </tr>
-              ))}
-            </table>
-          </div>
-        ))}
-      </div>
+    <div style={{ height: "100vh" }}>
       <button
         onClick={async () => {
           // Create a World contract instance
@@ -89,8 +153,11 @@ export const GameBoard = () => {
           await txResult.wait();
         }}
       >
-        forward
+        HARVEST
       </button>
+      <Canvas orthographic camera={{ zoom: 40, position: [-1, 1, -1] }}>
+        <Scene />
+      </Canvas>
     </div>
   );
 };
